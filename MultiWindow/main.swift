196f4f48f1ce0f29e973a5583c9abdc3cfeb24f4
LS Hung <61226932+lokshunhung@users.noreply.github.com>
2023-11-29 22:32:19 +0800

    Delete Storyboard

---
+++ MultiWindow/main.swift
@@ -0,0 +1,15 @@
+//
+//  main.swift
+//  MultiWindow
+//
+//  Created by LS Hung on 29/11/2023.
+//
+
+import UIKit
+
+UIApplicationMain(
+    CommandLine.argc,
+    CommandLine.unsafeArgv,
+    nil,
+    NSStringFromClass(AppDelegate.self)
+)
