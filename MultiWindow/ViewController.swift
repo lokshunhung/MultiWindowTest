ade6a2534ca6af1b4c446574555c435933294197
LS Hung <61226932+lokshunhung@users.noreply.github.com>
2023-11-29 22:53:26 +0800

    Add button to view controller

--- MultiWindow/ViewController.swift
+++ MultiWindow/ViewController.swift
@@ -6,13 +6,44 @@
 //
 
 import UIKit
+import os.log
 
 class ViewController: UIViewController {
 
     override func viewDidLoad() {
         super.viewDidLoad()
 
-        view.backgroundColor = .red
+        view.backgroundColor = .systemBackground
+        title = "View Controller"
+
+        let button = newButton {
+            Logger.viewController.info("clicked")
+        }
+        view.addSubview(button)
+
+        NSLayoutConstraint.activate([
+            view.centerXAnchor.constraint(equalTo: button.centerXAnchor),
+            view.centerYAnchor.constraint(equalTo: button.centerYAnchor),
+            button.widthAnchor.constraint(equalToConstant: 200),
+            button.heightAnchor.constraint(equalToConstant: 50),
+        ])
+    }
+
+    private func newButton(perform: @escaping () -> Void) -> some UIView {
+        let button = UIButton(
+            primaryAction: UIAction(handler: { action in perform() }))
+        button.translatesAutoresizingMaskIntoConstraints = false
+        var config: UIButton.Configuration = .filled()
+        config.title = "Click me"
+        button.configuration = config
+        return button
     }
 
 }
+
+private extension Logger {
+    static let viewController: Self = .init(
+        subsystem: Bundle.main.bundleIdentifier!,
+        category: "viewController")
+}
+
